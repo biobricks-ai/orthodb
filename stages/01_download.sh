24e7919745440e48190d6fe199f2f16228e93a96
Zakariyya Mughal <zaki.mughal@gmail.com>
2025-09-04 13:56:57 -0400

    fix(download): Extract URLs from list directly
    With the previous approach, `wget` downloads THEN rejects the large
    FASTA files.

--- stages/01_download.sh
+++ stages/01_download.sh
@@ -19,13 +19,16 @@ cd $downloadpath;
 
 # Retrieve the list of files to download from FTP base address
 # NOTE: Not downloading the larger FASTA files for now.
-wget -P "$downloadpath" \
-  --mirror -np \
-  -A '*.tab.gz,*.txt' \
-  --no-host-directories \
-  --cut-dirs=2 \
-  -B "$URL" \
-  --force-html \
-  -i "$listpath/index.html"
+grep -oP 'https?\S+' "$listpath"/index.html \
+	| grep -vP '.*_fasta\.t?gz$' \
+	| grep -P '.*(\.tab\.gz|\.txt)$' \
+	| wget -P "$downloadpath" \
+		--mirror -np \
+		-A '*.tab.gz,*.txt' \
+		-R '*_fasta.gz,*_fasta.tgz' \
+		--no-host-directories \
+		--cut-dirs=2 \
+		-B "$URL" \
+		-i -
 
 echo "Download done."
